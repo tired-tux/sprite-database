9bbfd77f04bb7d2bbe694c3337f16ea646542d65
Eli Thrash <vmlinuz0hio@gmail.com>
2023-06-11 13:40:07 -0500

    Adding a new location!

--- src/pu.sh
+++ src/pu.sh
@@ -5,10 +5,9 @@ mkdir $2
 cd $2
 mkdir $3
 cd $3
-touch $4
-cargo r > $4
-cat $4
+touch $4.md
+cargo r > $4.md
+cat $4.md
 git add ~/sprite-database/*
 git commit -m "Adding a new location!"
 git push
-rm tmp*
