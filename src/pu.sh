955589c94a9d1bc6c0981e7f0639214097cb8ec0
Eli Thrash <vmlinuz0hio@gmail.com>
2023-06-11 13:12:13 -0500

    Adding a new location!

--- src/pu.sh
+++ src/pu.sh
@@ -1,11 +1,9 @@
 #!/bin/zsh
-
-cargo r > .tmp
 mkdir ~/sprite-database/entries/$1/$2/$3/
 touch ~/sprite-database/entries/$1/$2/$3/$4
-cat .tmp | tee ~/sprite-database/entries/$1/$2/$3/$4
-rm *tmp*
+cargo r > ~/sprite-database/entries/$1/$2/$3/$4
 cat ~/sprite-database/entries/$1/$2/$3/$4
 git add ~/sprite-database/*
 git commit -m "Adding a new location!"
 git push
+rm tmp*
