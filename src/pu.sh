411c27aada8a94ed90fd7c3e07c0d8b069213d1f
Eli Thrash <vmlinuz0hio@gmail.com>
2023-06-11 12:53:50 -0500

    Adding a new location!

--- src/pu.sh
+++ src/pu.sh
@@ -4,5 +4,6 @@ touch ~/sprite-database/entries/$1
 cat .tmp | tee ~/sprite-database/entries/$1
 rm .tmp
 cat ~/sprite-database/entries/$1
-git add *
-git commit -m "Adding new Rating"
+git add ~/sprite-database/*
+git commit -m "Adding a new location!"
+git push
